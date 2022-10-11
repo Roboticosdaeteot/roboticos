42fd1fc3ddff0b4b02f748ad405ba9a6b22d2b3c
Lucas Poeys Carpenter de Sant'Anna <92745389+Lucas-Poeys@users.noreply.github.com>
2022-10-10 23:01:16 -0300

    Atualização
    -Terminando Script do jogo
    -Adicionando imagens complementares

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-const robo = document.querySelector('.robo');
 const virus = document.querySelector('.virus');
+let vivo = document.getElementById('vivo');
+let morte = document.getElementById('morte');
 
 const pulo = () => {
-    robo.classList.add('pulo');
+    vivo.classList.add('pulo');
 
     setTimeout(() => {
-    robo.classList.remove('pulo')
+    vivo.classList.remove('pulo')
     }, 500);
 }
 
 const loop = setInterval(()=>{
 
     const virusPosition = virus.offsetLeft;
-    const roboPosition = +window.getComputedStyle(robo).botton.replace('px', '');
+    const roboPosition = +window.getComputedStyle(vivo).bottom.replace("px","");
 
-    console.log(roboPosition);
-
-    if (virusPosition <= 80 && virusPosition > 0 && roboPosition < 75){ // tamanho de quando bate ele para
+    if (virusPosition <= 90 && virusPosition > 0 && roboPosition < 90){ // tamanho de quando bate ele para
         
       virus.style.animation = 'none';  
       virus.style.left = `${virusPosition}px`;
 
+      vivo.style.animation = 'none';
+      vivo.style.bottom = `${roboPosition}px`;
+      
+      morte.style.display = "block";
+      morte.style.bottom = `${roboPosition}px`;
+      vivo.style.display = "none";
+
+      clearInterval(loop);
     }
 
 }, 10);
